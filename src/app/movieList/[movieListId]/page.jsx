36cbd69c7a993660601befd04eadfbe2e2eed62b
Fadhil <musyaffa262@gmail.com>
2023-11-26 20:55:42 +0700

    Fixed ???

--- src/app/movieList/[movieListId]/page.jsx
+++ src/app/movieList/[movieListId]/page.jsx
@@ -32,39 +32,6 @@ export default function test() {
   return (
     <div>
       <div className="mt-[5rem]">
-<<<<<<< Updated upstream
-        <div>
-          <div className="">
-            <section
-              style={{ backgroundImage: `url(/img/bg.jpg)` }}
-              className="bg-center bg-no-repeat bg-blend-multiply"
-            >
-              <div className="max-w-screen-xl px-4 py-24 mx-auto text-center lg:py-20 cursor-default">
-                <h1 className="mb-4 text-2xl font-bold leading-none tracking-tight text-white md:text-5xl lg:text-3xl">
-                  {item.title}
-                </h1>
-                <div className="flex justify-center">
-                  <Image
-                    className="items-baseline justify-center mb-4 bg-cover rounded-lg"
-                    width={200}
-                    height={200}
-                    src={`${process.env.NEXT_PUBLIC_TMDB_BASEIMGURL}/${item.poster_path}`}
-                    alt=""
-                  />
-                </div>
-
-                <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
-                  <div className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white bg-[#DB2887] rounded-lg  gap-x-2">
-                    <FaStar />
-                    {item.vote_average}
-                  </div>
-                  <div className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white bg-[#DB2887] rounded-lg hover:text-gray-900 sm:ms-4 hover:bg-gray-100 focus:ring-4 focus:ring-gray-400 gap-x-2">
-                    <MdAccessTimeFilled />
-                    {item.runtime} min
-                  </div>
-                </div>
-                <div className="flex flex-col mt-5 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 gap-x-4">
-=======
         <div
           className="absolute inset-0"
           style={{
@@ -87,7 +54,6 @@ export default function test() {
               <div className="flex flex-col pr-5 ml-6 space-y-4 ">
                 <h1 className="text-2xl font-bold sm:text-7xl">{item.title}</h1>
                 <div className="flex flex-wrap">
->>>>>>> Stashed changes
                   {item.genres?.map((genre, key) => (
                     <div
                       key={key}
